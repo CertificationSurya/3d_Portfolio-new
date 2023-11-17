f0719694d7f856ecd18f4ace29f62f889fe3700e
certificationSurya - Pc <certification.surya@gmail.com>
2023-11-17 22:37:33 +0545

    Home and Contact Form, almost done

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,157 @@
+import { Suspense, useRef, useState } from "react";
+import emailjs from "@emailjs/browser";
+import { Canvas } from "@react-three/fiber";
+import Loader from "../components/Loader";
+import Fox from "../models/Fox";
+
+const initialFormData = {
+  name: "",
+  email: "",
+  message: "",
+};
+
+const Contact = () => {
+  const formRef = useRef(null);
+  const [form, setForm] = useState(initialFormData);
+  const [isLoading, setIsLoading] = useState(false);
+
+  // animation state
+  const [currentAnimation, setCurrentAnimation] = useState("idle");
+
+  const handleChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+    setCurrentAnimation("hit");
+
+    emailjs
+      .send(
+        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
+        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+        {
+          from_name: form.name,
+          to_name: "Certification Surya",
+          from_email: form.email,
+          to_email: "certification.surya@gmail.com",
+          message: form.message,
+        },
+        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+      )
+      .then((res) => {
+        setIsLoading(false);
+        // TODO: show success message
+        // TODO: hide an alert
+
+        setTimeout(()=> {
+          setCurrentAnimation('idle')
+          setForm(initialFormData);
+        },2500)
+      })
+      .catch((err) => {
+        setIsLoading(false);
+        console.log(err);
+        setCurrentAnimation("idle");
+        // TODO: show error message
+      });
+  };
+  const handleFocus = () => setCurrentAnimation("walk");
+  const handleBlur = () => setCurrentAnimation("idle");
+
+  return (
+    <section className="relative flex lg:flex-row flex-col max-container">
+      <div className="flex-1 min-w-[50%] flex flex-col">
+        <h1 className="head-text">Get in Touch</h1>
+
+        <form
+          className="w-full flex flex-col gap-4 mt-14"
+          onSubmit={handleSubmit}
+        >
+          <label htmlFor="name" className="text-black-500 font-semibold">
+            Name
+            <input
+              type="text"
+              autoComplete="true"
+              id="name"
+              name="name"
+              value={form.name}
+              onChange={handleChange}
+              onFocus={handleFocus}
+              onBlur={handleBlur}
+              className="input"
+              placeholder="John Doe"
+              required
+            />
+          </label>
+
+          <label htmlFor="email" className="text-black-500 font-semibold">
+            Email
+            <input
+              autoComplete="true"
+              type="email"
+              id="email"
+              name="email"
+              value={form.email}
+              onChange={handleChange}
+              onFocus={handleFocus}
+              onBlur={handleBlur}
+              className="input"
+              placeholder="John@gmail.com"
+              required
+            />
+          </label>
+
+          <label htmlFor="message" className="text-black-500 font-semibold">
+            Your Message
+            <textarea
+              name="message"
+              id="message"
+              rows="4"
+              className="textarea"
+              placeholder="Write your thoughts here..."
+              value={form.message}
+              onChange={handleChange}
+              onFocus={handleFocus}
+              onBlur={handleBlur}
+            />
+          </label>
+
+          <button
+            type="submit"
+            disabled={isLoading}
+            className="btn"
+            onFocus={handleFocus}
+            onBlur={handleBlur}
+          >
+            {isLoading ? "Sending..." : "Send Message"}
+          </button>
+        </form>
+      </div>
+
+      <div className="lg:w-1/2 w-full lg:h-auto md:h-[550px] h-[350px]">
+        <Canvas
+          camera={{
+            position: [0, 0, 5],
+            fov: 75,
+            near: 0.1,
+            far: 1000,
+          }}
+        >
+          <directionalLight intensity={2.5} position={[0, 0, 1]} />
+          <ambientLight intensity={0.5} />
+          <Suspense fallback={<Loader />}>
+            <Fox
+              currentAnimation={currentAnimation}
+              position={[0.5, 0.35, 0]}
+              rotation={[12.6, -0.6, 0]}
+              scale={[0.5, 0.5, 0.5]}
+            />
+          </Suspense>
+        </Canvas>
+      </div>
+    </section>
+  );
+};
+
+export default Contact;
